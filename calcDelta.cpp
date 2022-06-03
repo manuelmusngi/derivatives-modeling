80b797de03de983ebfb15d0015db18c4b09f82a2
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-06-03 09:49:22 -0400

    Add files via upload

---
+++ calcDelta.cpp
@@ -0,0 +1,33 @@
+/*
+calcDelta                :Delta calculation of an Option
+
+[in>>]   double price    :underlying price
+         double strike   :strike price
+         double rate     :interest rate
+         double div      :dividend yield
+         double T        :time to maturity or days until expiration
+         char type       :Call Option 'C' or Put Option 'P'
+[out<<]  double          :Delta
+*/
+#include <cmath>
+#include <string>
+
+double calcDelta(double price,
+                 double strike,
+                 double rate,
+                 double div,
+                 double vol,
+                 double T,
+                char type)
+{
+    double d1, delta;
+    
+    d1 = (log(price) + (rate - div + (vol*vol/2))*T) / (vol*sqrt(T));
+    
+    if (type == 'C')
+        delta = exp(-div*T)*util.normalCalcPrime(d1);
+    else
+        delta = exp(-div*T)*(util.normalCalc(d1) - 1);
+    
+    return delta;
+};
