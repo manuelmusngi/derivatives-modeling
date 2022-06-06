c0fe38c481b8f59a614c30cae102b221fec20aac
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-06-06 10:35:08 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,25 +7,25 @@
 
 int main(int argc, char **argv) {
 
-    // First we create the parameter list
+    // Parameter list with example values
     double S = 100.0;  // Underlying spot price
     double K = 100.0;  // Strike price
     double r = 0.05;   // Risk-free rate (5%)
-    double T = 1.0;    // One year until expiry
+    double T = 1.0;    // One year until option expiration
     double C_M = 10.5; // Option market price
 
-    // Create the Black-Scholes Call functor
+    // Black-Scholes Call parameters
     BlackScholesCall bsc(S, K, r, T);
 
-    // Newton Raphson parameters
+    // Newton-Raphson parameters
     double init = 0.3;  // Our guess impl. vol of 30%
     double epsilon = 0.001;
 
-    // Calculate the implied volatility
+    // Calculate the Implied Volatility
     double sigma = newton_raphson<BlackScholesCall, &BlackScholesCall::option_price, 
                                   &BlackScholesCall::option_vega>(C_M, init, epsilon, bsc);
 
-    // Output the values
+    // Output values
     std::cout << "Implied Vol: " << sigma << std::endl;
 
     return 0;
