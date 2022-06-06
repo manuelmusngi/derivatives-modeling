70c67620d339d9514ccd785ad5e527ddd25dd183
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-06-06 10:36:43 -0400

    Add files via upload

---
+++ black_scholes.cpp
@@ -0,0 +1,19 @@
+#ifndef __BLACK_SCHOLES_CPP
+#define __BLACK_SCHOLES_CPP
+
+#include "black_scholes.h"
+#include "bs_prices.h"
+
+BlackScholesCall::BlackScholesCall(double _S, double _K, 
+								   double _r, double _T) :
+	S(_S), K(_K), r(_r), T(_T) {}
+
+double BlackScholesCall::option_price(double sigma) const {
+	return call_price(S, K, r, sigma, T);
+}
+
+double BlackScholesCall::option_vega(double sigma) const {
+  return call_vega(S, K, r, sigma, T);
+}
+
+#endif
