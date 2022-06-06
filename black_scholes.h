0836a92e8ff66d9c5fd6366e38d6240e40a9b4c2
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-06-06 10:43:57 -0400

    Add files via upload

---
+++ black_scholes.h
@@ -0,0 +1,18 @@
+#ifndef __BLACK_SCHOLES_H
+#define __BLACK_SCHOLES_H
+
+class BlackScholesCall {
+private:
+	double S; 		// Underlying asset price
+	double K;		// Strike price
+	double r;		// Risk-free rate
+	double T; 		// Time to maturity
+
+public:
+	BlackScholesCall(double _S, double _K, 
+					 double _r, double _T);
+
+	double option_price(double sigma) const;
+        double option_vega(double sigma) const;
+};
+#endif 
