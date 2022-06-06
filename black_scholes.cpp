5c4dea8c0d1acea7d77ef6fb31b99514d818bb13
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-06-06 17:19:41 -0400

    Update black_scholes.cpp

--- black_scholes.cpp
+++ black_scholes.cpp
@@ -7,14 +7,14 @@
 BlackScholesCall::BlackScholesCall(double _S, double _K, double _r, double _T) :
     S(_S), K(_K), r(_r), T(_T) {}
 
-double BlackScholesCall::option_price(double sigma) const 
-{
-   return call_price(S, K, r, sigma, T);
-}
+    double BlackScholesCall::option_price(double sigma) const 
+    {
+       return call_price(S, K, r, sigma, T);
+    }
 
-double BlackScholesCall::option_vega(double sigma) const 
-{
-  return call_vega(S, K, r, sigma, T);
-}
+    double BlackScholesCall::option_vega(double sigma) const 
+    {
+      return call_vega(S, K, r, sigma, T);
+    }
 
 #endif
