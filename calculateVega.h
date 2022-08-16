ca91974317ec6a79ad9ae2b4a4abfdbad1fea268
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-08-16 10:47:55 -0400

    Update calculateVega.h

--- calculateVega.h
+++ calculateVega.h
@@ -13,6 +13,6 @@ double calculateVega(double price,double strike, double rate, double vol, double
 {
 	double d1 = (log(price/strike) + (rate + (vol)*(vol)/2)*T)/(vol*sqrt(T));
 	return price*sqrt(T)*normalCalc(d1);
-}
+};
 
 #endif
