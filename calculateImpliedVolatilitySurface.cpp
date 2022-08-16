30c9a82cd633215e2003ec143c29d65f94a15b40
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-08-16 10:36:18 -0400

    Update calculateImpliedVolatilitySurface.cpp

--- calculateImpliedVolatilitySurface.cpp
+++ calculateImpliedVolatilitySurface.cpp
@@ -16,26 +16,26 @@
 using namespace std;
 
 map<pair<double, int>, double> calculateImpliedVolatilitySurface(double price,
-													 			 vector<double> optionPrices, 
-													 			 vector<int> strikes, 
-													 			 vector<double> maturities,
-													 			 map<double, double> rates)
+								 vector<double> optionPrices, 
+								 vector<int> strikes, 
+								 vector<double> maturities,
+								 map<double, double> rates)
 {
-	map<pair<double, int>, double> surfaceMap;			// map strike and maturity to implied volatility
-	pair<double, int> TXPair; 							// time (expiration)  - strike pair
-	vector<pair<double, int> > vecPair; 				// vector of TXPairs
+	map<pair<double, int>, double> surfaceMap;		// map strike and maturity to implied volatility
+	pair<double, int> TXPair; 				// time (expiration)  - strike pair
+	vector<pair<double, int> > vecPair; 			// vector of TXPairs
 	vector<pair<double, int> >::iterator vecPairIter; 	// vector map iterator
-	vector<double>::iterator priceIter; 				// vector price iterator
+	vector<double>::iterator priceIter; 			// vector price iterator
 	int j = 0;
 	int cnt = 0;
-	const double epsilon = 0.000001; 					// error tolerance
-	double error = 0.0; 								// error between market and model prices
-	double vol1 = 0.0; 									// implied volatility
-	double vol2 = 0.0; 									// temporary volatility
-	double vega = 0.0; 									// option vega
-	double BSPrice = 0.0; 								// black scholes pricing
-	double marketPrice = 0.0; 							// market price of instrument
-	int* strike = new int[strikes.size()]; 				// array of strikes
+	const double epsilon = 0.000001; 			// error tolerance
+	double error = 0.0; 					// error between market and model prices
+	double vol1 = 0.0; 					// implied volatility
+	double vol2 = 0.0; 					// temporary volatility
+	double vega = 0.0; 					// option vega
+	double BSPrice = 0.0; 					// black scholes pricing
+	double marketPrice = 0.0; 				// market price of instrument
+	int* strike = new int[strikes.size()]; 			// array of strikes
 	double* maturity = new double[maturities.size()]; 	// array of maturities or expiration
 	double* call = new double[optionPrices.size()]; 	// array of call prices
 	
@@ -47,7 +47,7 @@ map<pair<double, int>, double> calculateImpliedVolatilitySurface(double price,
 	copy(maturities.begin(), maturities.end(), maturity);
 
 
-	if (type == "C")     // "C" for Call
+	if (type == "C")   // "C" for Call
 	{
 		for (priceIter = optionPrices.begin(); priceIter != optionPrices.end(); priceIter++)
 		{
@@ -73,10 +73,10 @@ map<pair<double, int>, double> calculateImpliedVolatilitySurface(double price,
 		for (priceIter = optionPrices.begin(); priceIter != optionPrices.end(); priceIter++)
 		{
 			marketPrice = *priceIter;
-			vol1 = 0.55; 					// initial assumed volatility for Newton-Raphson
+			vol1 = 0.55; 	// initial assumed volatility for Newton-Raphson
 			do
 			{
-				BSPrice = BSPutPrice(vol1, rates[maturity[cnt]], strike[cnt], price, T);
+			  	BSPrice = BSPutPrice(vol1, rates[maturity[cnt]], strike[cnt], price, T);
 				vega = calculateVega(price, strike[cnt], rates[maturity[cnt]], vol1, T);
 				vol2 = vol1 - (BSPrice - marketPrice) / (vega);
 				error = vol2 - vol1;
@@ -88,10 +88,10 @@ map<pair<double, int>, double> calculateImpliedVolatilitySurface(double price,
 				cnt++;
 		}
 	}
-		// print out implied volatility
+		// printout of implied volatility surface
 		for (vecPairIter = vecPair.begin(); vecPairIter != vecPair.end(); vecPairIter++)
 			cout << surfaceMap[*vecPairIter] << endl;
 
-			return surfaceMap;
+		return surfaceMap;
 }
 
