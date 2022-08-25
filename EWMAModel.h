14541f16c61d344b532dccf1f60189e27a1aed8f
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-08-25 17:35:35 -0400

    Add files via upload

---
+++ EWMAModel.h
@@ -0,0 +1,26 @@
+#ifndef EWMAMODEL_H
+#define EWMAMODEL_H
+
+#include <vector>
+#include <utility>
+#include <cmath>
+#include <numeric>
+
+using namespace std;
+
+class EWMAModel
+{
+	protected:
+		vector<double> returns;
+		vector<double> S; // price of instrument
+		int T;
+		int i;
+
+	public:
+		int EWMA() {}
+		virtual ~EWMAModel() {};
+		vector<double> EWMA(double // computes exponential moving average
+			lambda, vector<double>& prices);
+};
+
+#endif
