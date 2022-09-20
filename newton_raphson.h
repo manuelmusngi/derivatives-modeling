f2e94bc833b5a652c2971f8fd1e38a4b02e594ac
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2022-09-20 11:46:03 -0400

    Update newton_raphson.h

--- newton_raphson.h
+++ newton_raphson.h
@@ -3,24 +3,28 @@
 
 #include <cmath>
 
+using namespace std;
+
 template<typename T,
     double (T::* g)(double) const,
     double (T::* g_prime)(double) const>
+    
 double newton_raphson(double y_target,       // Target y value
-    double init,           // Initial x value
-    double epsilon,        // Tolerance
-    const T& root_func) {  // Function objec
-// 
-    double y = (root_func.*g)(init);
-    double x = init;
+                      double init,           // Initial x value
+                      double epsilon,        // Tolerance
+                      const T& root_func)                                              
+{                                            // Set initial options prices and volatility
+double y = (root_func.*g)(init);             // initial option prices
+double x = init;                             // initial volatility
 
-    //
-    while (fabs(y - y_target) > epsilon) {
-        double d_x = (root_func.*g_prime)(x);
-        x += (y_target - y) / d_x;
-        y = (root_func.*g)(x);
-    }
-    return x;
+while (fabs(y - y_target) > epsilon) 
+{
+  double d_x = (root_func.*g_prime)(x);
+  x += (y_target - y) / d_x;
+  y = (root_func.*g)(x);
+}
+  return x;
 }
 
 #endif
+
