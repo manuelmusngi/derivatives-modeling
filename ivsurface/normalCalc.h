5b3d85a0d987c9428846869a767fd2f39ec0f761
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2023-02-06 18:41:12 -0500

    Update normalCalc.h

--- ivsurface/normalCalc.h
+++ ivsurface/normalCalc.h
@@ -11,18 +11,19 @@
 
 double normalCalc(double d)
 {
-	const double a1 = 0.319381530;
-	const double a2 = -0.356563782;
-	const double a3 = 1.781477937;
-	const double a4 = -1.821255978;
-	const double a5 = 1.330274429;
-	const double gamma = 0.2316419;
-	const double PI = 3.14159;
-	const double k1 = 1 / (1 + gamma * d);
-	const double k2 = 1 / (1 - gamma * d);
-	const double normalprime = (1 / (sqrt(2 * PI))) * exp(-d * d / 2);
-	double value = 0.0;
-	double h = 0.0;
+	const auto a1 = 0.319381530;
+	const auto a2 = -0.356563782;
+	const auto a3 = 1.781477937;
+	const auto a4 = -1.821255978;
+	const auto a5 = 1.330274429;
+	const auto gamma = 0.2316419;
+	const auto PI = 3.14159;
+	const auto k1 = 1 / (1 + gamma * d);
+	const auto k2 = 1 / (1 - gamma * d);
+	const auto normalprime = (1 / (sqrt(2 * PI))) * exp(-d * d / 2);
+	auto value = 0.0;
+	auto h = 0.0;
+	
 	if (d >= 0)
 		value = 1 - normalprime * (a1 * k1 + a2 * pow(k1, 2) + a3 * pow(k1, 3) + a4 * pow(k1, 4) +
 			a5 * pow(k1, 5));
