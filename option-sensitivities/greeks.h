97125a51381cccb7d651b928cad3c1f70bd3e3f0
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2023-02-09 18:24:18 -0500

    Update greeks.h

--- option-sensitivities/greeks.h
+++ option-sensitivities/greeks.h
@@ -7,51 +7,39 @@
 #include <numeric>
 
 // Standard normal probability density function
-double norm_pdf(const double x);
+double normPDF(const double x);
 
 // Approximations to the cumulative distribution function(cdf) for the standard normal distribution(pdf)
-double norm_cdf(const double x);
+double normCDF(const double x);
 
-// This calculates d_j, for j in {1,2}. This term appears in the closed form solution for the European Call or Put price
+// This calculates d_j, for j in {1,2}. 
+// This term appears in the closed form solution for the European Call or Put price
 double d_j(const int j, const double S, const double K, const double r, const double v, const double T);
 
-// Calculate the European Call price based on:
-// underlying S, strike K, risk-free rate r, volatility of underlying sigma and time to maturity T
-double call_price(const double S, const double K, const double r, const double v, const double T);
+/* Calculate Option Greek based on:
+   underlying S, 
+   strike K, 
+   risk-free rate r, 
+   volatility of underlying v 
+   time to expiration T
+*/
 
-// Calculate the European Call Delta
-double call_delta(const double S, const double K, const double r, const double v, const double T);
+// Call Option
+double callPrice(const double S, const double K, const double r, const double v, const double T);
+double callDelta(const double S, const double K, const double r, const double v, const double T);
+double callGamma(const double S, const double K, const double r, const double v, const double T);
+double callVega(const double S, const double K, const double r, const double v, const double T);
+double callTheta(const double S, const double K, const double r, const double v, const double T);
+double callRho(const double S, const double K, const double r, const double v, const double T);
 
-// Calculate the European Call Gamma
-double call_gamma(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Call Vega
-double call_vega(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Call Theta
-double call_theta(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Call Rho
-double call_rho(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put price based on:
-// underlying S, strike K, risk-free rate r, volatility of underlying sigma and time to maturity T
-double put_price(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put Delta
-double put_delta(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put Gamma
-double put_gamma(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put Vega
-double put_vega(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put Theta
-double put_theta(const double S, const double K, const double r, const double v, const double T);
-
-// Calculate the European Put Rho
-double put_rho(const double S, const double K, const double r, const double v, const double T);
+// Put Option
+double putPrice(const double S, const double K, const double r, const double v, const double T);
+double putDelta(const double S, const double K, const double r, const double v, const double T);
+double putGamma(const double S, const double K, const double r, const double v, const double T);
+double putVega(const double S, const double K, const double r, const double v, const double T);
+double putTheta(const double S, const double K, const double r, const double v, const double T);
+double putRho(const double S, const double K, const double r, const double v, const double T);
 
 #endif
+
 
