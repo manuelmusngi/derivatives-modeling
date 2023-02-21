956176c74d2ec8b7c1fe5e62b2f2d72625fb6950
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2023-02-21 09:57:42 -0500

    Update main.cpp

--- option-sensitivities/main.cpp
+++ option-sensitivities/main.cpp
@@ -6,7 +6,7 @@
 #include "greeks.h"
 #include "greeks.cpp"
 
-using namespace std;
+// using namespace std;
 
 int main(int argc, char** argv) 
 {
@@ -18,40 +18,40 @@ int main(int argc, char** argv)
 	auto T = 1.0;    // One year until expiration
 
 	// Calculate the Call/Put values and Options Greeks
-	auto call = callPrice(S, K, r, v, T);
-	auto call_delta_v = callDelta(S, K, r, v, T);
-	auto call_gamma_v = callGamma(S, K, r, v, T);
-	auto call_vega_v = callVega(S, K, r, v, T);
-	auto call_theta_v = callTheta(S, K, r, v, T);
-	auto call_rho_v = callRho(S, K, r, v, T);
+	double call = callPrice(S, K, r, v, T);
+	double call_delta_v = callDelta(S, K, r, v, T);
+	double call_gamma_v = callGamma(S, K, r, v, T);
+	double call_vega_v = callVega(S, K, r, v, T);
+	double call_theta_v = callTheta(S, K, r, v, T);
+	double call_rho_v = callRho(S, K, r, v, T);
 
-	auto put = putPrice(S, K, r, v, T);
-	auto put_delta_v = putDelta(S, K, r, v, T);
-	auto put_gamma_v = putGamma(S, K, r, v, T);
-	auto put_vega_v = putVega(S, K, r, v, T);
-	auto put_theta_v = putTheta(S, K, r, v, T);
-	auto put_rho_v = putRho(S, K, r, v, T);
+	double put = putPrice(S, K, r, v, T);
+	double put_delta_v = putDelta(S, K, r, v, T);
+	double put_gamma_v = putGamma(S, K, r, v, T);
+	double put_vega_v = putVega(S, K, r, v, T);
+	double put_theta_v = putTheta(S, K, r, v, T);
+	double put_rho_v = putRho(S, K, r, v, T);
 
 	// Output Greek parameters and Option price
-	cout << "Underlying:      " << S << endl;
-	cout << "Strike:          " << K << endl;
-	cout << "Risk-Free Rate:  " << r << endl;
-	cout << "Volatility:      " << v << endl;
-	cout << "Expiration:      " << T << endl << endl;
+	std::cout << "Underlying:      " << S << std::endl;
+	std::cout << "Strike:          " << K << std::endl;
+	std::cout << "Risk-Free Rate:  " << r << std::endl;
+	std::cout << "Volatility:      " << v << std::endl;
+	std::cout << "Expiration:      " << T << std::endl << std::endl;
 
-	cout << "Call Price:      " << call << endl;
-	cout << "Call Delta:      " << call_delta_v << endl;
-	cout << "Call Gamma:      " << call_gamma_v << endl;
-	cout << "Call Vega:       " << call_vega_v << endl;
-	cout << "Call Theta:      " << call_theta_v << endl;
-	cout << "Call Rho:        " << call_rho_v << endl << endl;
+	std::cout << "Call Price:      " << call << std::endl;
+	std::cout << "Call Delta:      " << call_delta_v << std::endl;
+	std::cout << "Call Gamma:      " << call_gamma_v << std::endl;
+	std::cout << "Call Vega:       " << call_vega_v << std::endl;
+	std::cout << "Call Theta:      " << call_theta_v << std::endl;
+	std::cout << "Call Rho:        " << call_rho_v << std::endl << std::endl;
 
-	cout << "Put Price:       " << put << endl;
-	cout << "Put Delta:       " << put_delta_v << endl;
-	cout << "Put Gamma:       " << put_gamma_v << endl;
-	cout << "Put Vega:        " << put_vega_v << endl;
-	cout << "Put Theta:       " << put_theta_v << endl;
-	cout << "Put Rho:         " << put_rho_v << endl;
+	std::cout << "Put Price:       " << put << std::endl;
+	std::cout << "Put Delta:       " << put_delta_v << std::endl;
+	std::cout << "Put Gamma:       " << put_gamma_v << std::endl;
+	std::cout << "Put Vega:        " << put_vega_v << std::endl;
+	std::cout << "Put Theta:       " << put_theta_v << std::endl;
+	std::cout << "Put Rho:         " << put_rho_v << std::endl;
 
 	return 0;
 }
