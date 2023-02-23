9bebb1b6cebeee64ae8aeb9b20c5409d1e612c36
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2023-02-23 16:18:32 -0500

    Update newton_raphson.h (#5)
    Update with std::fabs

--- newton-raphson/newton_raphson.h
+++ newton-raphson/newton_raphson.h
@@ -2,29 +2,27 @@
 #define __NEWTON_RAPHSON_H
 
 #include <cmath>
+#include <numeric>
 
-using namespace std;
+template<typename T, 
+  double (T::*g)(double) const,
+  double (T::*g_prime)(double) const>
+  double newton_raphson(double y_target,       // Target y value
+                        double init,           // Initial x value
+                        double epsilon,        // Tolerance
+                        const T& root_func)    //
+{   
+  double y = (root_func.*g)(init);
+  double x = init;
 
-template<typename T,
-    double (T::* g)(double) const,
-    double (T::* g_prime)(double) const>
-    
-double newton_raphson(double y_target,       // Target y value
-                      double init,           // Initial x value
-                      double epsilon,        // Tolerance
-                      const T& root_func)                                              
-{                                            // Set initial options prices and volatility
-double y = (root_func.*g)(init);             // initial option prices
-double x = init;                             // initial volatility
-
-while (fabs(y - y_target) > epsilon) 
-{
-  double d_x = (root_func.*g_prime)(x);
-  x += (y_target - y) / d_x;
-  y = (root_func.*g)(x);
-}
+  //
+  while (std::fabs(y-y_target) > epsilon) 
+  {
+    double d_x = (root_func.*g_prime)(x);
+    x += (y_target-y)/d_x;
+    y = (root_func.*g)(x);
+  }
   return x;
 }
 
-#endif
 
