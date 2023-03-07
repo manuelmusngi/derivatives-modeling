349e65df066cec33e6c29bf7752aec34eb471abc
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2023-03-07 15:17:16 -0500

    Add files via upload

---
+++ option-sensitivities/BlackScholesModel.h
@@ -0,0 +1,42 @@
+#ifndef BLACKSCHOLESMODEL_H
+#define BLACKSCHOLESMODEL_H
+
+class BlackScholesModel 
+{
+    public:
+        // Constructor
+        BlackScholesModel(double spotPrice, 
+                          double strikePrice, 
+                          double riskFreeRate,
+                          double timeToExpiry, 
+                          double volatility);
+
+        // Accessor methods
+        double getDelta();
+        double getTheta();
+        double getGamma();
+        double getVega();
+        double getRho();
+
+    private:
+        // Input parameters
+        double spotPrice_;
+        double strikePrice_;
+        double riskFreeRate_;
+        double timeToExpiry_;
+        double volatility_;
+
+        // Helper functions
+        double d1();
+        double d2();
+        double normalCDF(double x);
+
+        // Greeks
+        double delta_;
+        double theta_;
+        double gamma_;
+        double vega_;
+        double rho_;
+};
+
+#endif 
