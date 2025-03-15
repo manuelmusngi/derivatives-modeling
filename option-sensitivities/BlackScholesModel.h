85fa0e7487d85e8d51998e0032d68262366e5fb3
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2025-03-15 17:04:53 -0400

    Update BlackScholesModel.h

--- option-sensitivities/BlackScholesModel.h
+++ option-sensitivities/BlackScholesModel.h
@@ -1,24 +1,35 @@
-#ifndef BLACKSCHOLESMODEL_H
-#define BLACKSCHOLESMODEL_H
+#ifndef BLACK_SCHOLES_MODEL_H
+#define BLACK_SCHOLES_MODEL_H
 
 #include <cmath> // For mathematical functions
 
+/**
+ * @class BlackScholesModel
+ * @brief A class to model the Black-Scholes options pricing model and calculate the Greeks.
+ */
 class BlackScholesModel 
 {
     public:
-        // Constructor
-        BlackScholesModel(double spotPrice, 
-                          double strikePrice, 
-                          double riskFreeRate,
-                          double timeToExpiry, 
-                          double volatility);
+        /**
+         * @brief Constructor for BlackScholesModel.
+         * @param spotPrice The current price of the underlying asset.
+         * @param strikePrice The strike price of the option.
+         * @param riskFreeRate The risk-free interest rate.
+         * @param timeToExpiry The time to expiry of the option.
+         * @param volatility The volatility of the underlying asset.
+         */
+        BlackScholesModel(double spotPrice = 100.0, 
+                          double strikePrice = 100.0, 
+                          double riskFreeRate = 0.05,
+                          double timeToExpiry = 1.0, 
+                          double volatility = 0.2);
 
         // Accessor methods
-        double getDelta();
-        double getTheta();
-        double getGamma();
-        double getVega();
-        double getRho();
+        double getDelta() const;
+        double getTheta() const;
+        double getGamma() const;
+        double getVega() const;
+        double getRho() const;
 
     private:
         // Input parameters
@@ -29,9 +40,9 @@ class BlackScholesModel
         double volatility_;
 
         // Helper functions
-        double d1();
-        double d2();
-        double normalCDF(double x);
+        double d1() const;
+        double d2() const;
+        double normalCDF(double x) const;
 
         // Greeks
         double delta_;
@@ -41,4 +52,4 @@ class BlackScholesModel
         double rho_;
 };
 
-#endif 
+#endif // BLACK_SCHOLES_MODEL_H
