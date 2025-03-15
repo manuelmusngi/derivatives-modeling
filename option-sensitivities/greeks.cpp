8e7745475bb98ab77919f0d5478c3acd58c96541
manuelmusngi <25419759+manuelmusngi@users.noreply.github.com>
2025-03-15 17:06:43 -0400

    Update greeks.cpp

--- option-sensitivities/greeks.cpp
+++ option-sensitivities/greeks.cpp
@@ -10,18 +10,26 @@ using namespace std;
 // Standard normal probability density function
 double normPDF(const double x)
 {
-    return (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x);
+    constexpr double inv_sqrt_2pi = 1.0 / sqrt(2 * M_PI);
+    return inv_sqrt_2pi * exp(-0.5 * x * x);
 }
 
-// Approximations to the cumulative distribution function(cdf) for the standard normal distribution(pdf)
+// Cumulative distribution function for the standard normal distribution
 double normCDF(const double x)
 {
-    double k = 1.0 / (1.0 + 0.2316419 * x);
-    double k_sum = k * (0.319381530 + k * (-0.356563782 + k * (1.781477937 + k * (-1.821255978 + 1.330274429 * k))));
+    constexpr double k1 = 0.2316419;
+    constexpr double k2 = 0.319381530;
+    constexpr double k3 = -0.356563782;
+    constexpr double k4 = 1.781477937;
+    constexpr double k5 = -1.821255978;
+    constexpr double k6 = 1.330274429;
+
+    double k = 1.0 / (1.0 + k1 * x);
+    double k_sum = k * (k2 + k * (k3 + k * (k4 + k * (k5 + k6 * k))));
 
     if (x >= 0.0)
     {
-        return (1.0 - (1.0 / (pow(2 * M_PI, 0.5))) * exp(-0.5 * x * x) * k_sum);
+        return 1.0 - normPDF(x) * k_sum;
     }
     else
     {
@@ -33,7 +41,7 @@ double normCDF(const double x)
 // This term appears in the closed form solution for the European Call or Put price
 double d_j(const int j, const double S, const double K, const double r, const double v, const double T)
 {
-    return (log(S / K) + (r + (pow(-1, j - 1)) * 0.5 * v * v) * T) / (v * (pow(T, 0.5)));
+    return (log(S / K) + (r + (j == 1 ? 0.5 : -0.5) * v * v) * T) / (v * sqrt(T));
 }
 
 // Call Option
